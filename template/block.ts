f713668c0daa895248f86ca4560da64af0a9635c
Ziphil <yfe23457@nifty.com>
2021-12-18 17:41:28 +0900

    定義リストの変換ルールを追加

--- template/block.ts
+++ template/block.ts
@@ -83,6 +83,40 @@ manager.registerElementRule("li", "page.ul", (transformer, document, element) =>
   return self;
 });
 
+manager.registerElementRule("el", "page", (transformer, document, element) => {
+  let self = document.createDocumentFragment();
+  self.appendElement("dl", (self) => {
+    self.addClassName("description-list");
+    self.setBlockType("text", "text");
+    self.appendChild(transformer.apply(element, "page.el"));
+  });
+  return self;
+});
+
+manager.registerElementRule("li", "page.el", (transformer, document, element) => {
+  let self = document.createDocumentFragment();
+  self.appendChild(transformer.apply(element, "page.el.li"));
+  return self;
+});
+
+manager.registerElementRule("et", "page.el.li", (transformer, document, element) => {
+  let self = document.createDocumentFragment();
+  self.appendElement("dt", (self) => {
+    self.addClassName("description-left");
+    self.appendChild(transformer.apply(element, "page"));
+  });
+  return self;
+});
+
+manager.registerElementRule("ed", "page.el.li", (transformer, document, element) => {
+  let self = document.createDocumentFragment();
+  self.appendElement("dd", (self) => {
+    self.addClassName("description-right");
+    self.appendChild(transformer.apply(element, "page"));
+  });
+  return self;
+});
+
 manager.registerElementRule("xl", "page", (transformer, document, element) => {
   let self = document.createDocumentFragment();
   self.appendElement("dl", (self) => {
