33424c164a9bb035b0d04f9e42afcc0916c10703
Ziphil <yfe23457@nifty.com>
2021-12-21 22:46:21 +0900

    ブロックコード関連の変換ルールを追加

--- template/block.ts
+++ template/block.ts
@@ -272,6 +272,41 @@ manager.registerElementRule(["th", "td"], "page.table.tr", (transformer, documen
   return self;
 });
 
+manager.registerElementRule(["pre", "samp"], "page", (transformer, document, element, scope, args) => {
+  let self = document.createDocumentFragment();
+  let lineTagName = (element.tagName === "pre") ? "code" : "samp";
+  let lines = element.textContent?.split("\n") ?? [];
+  let showNumber = element.tagName === "pre" && !element.hasAttribute("simple");
+  self.appendElement("div", (self) => {
+    self.addClassName("program-container");
+    self.setBlockType("bordered", "bordered");
+    self.setAttribute("data-type", element.tagName);
+    self.appendElement("div", (self) => {
+      self.addClassName("program-inner-container");
+      self.appendElement("div", (self) => {
+        self.addClassName("program");
+        if (showNumber) {
+          self.setAttribute("data-show-number", "");
+        }
+        for (let number = 0 ; number < lines.length ; number ++) {
+          let line = lines[number];
+          if (showNumber) {
+            self.appendElement("div", (self) => {
+              self.addClassName("program-number");
+              self.setAttribute("data-number", (number + 1).toString());
+            });
+          }
+          self.appendElement(lineTagName, (self) => {
+            self.addClassName("program-line");
+            self.appendTextNode(line);
+          });
+        }
+      });
+    });
+  });
+  return self;
+});
+
 manager.registerElementRule(["name", "ver", "use-script", "use-math"], "page", (transformer, document, element) => {
   let self = document.createDocumentFragment();
   return self;
