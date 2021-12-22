88a77b4e6a6b2cb02764768111da2e1351f481cd
Ziphil <yfe23457@nifty.com>
2021-12-22 19:14:03 +0900

    グロス関連の変換規則を追加

--- template/block.ts
+++ template/block.ts
@@ -189,6 +189,63 @@ manager.registerElementRule("ja", "page.xl.li", (transformer, document, element)
   return self;
 });
 
+manager.registerElementRule("gloss", "page", (transformer, document, element) => {
+  let self = document.createDocumentFragment();
+  self.appendElement("div", (self) => {
+    self.addClassName("gloss");
+    self.setBlockType("text", "text");
+    self.appendChild(transformer.apply(element, "page.gloss"));
+  });
+  return self;
+});
+
+manager.registerElementRule("li", "page.gloss", (transformer, document, element) => {
+  let self = document.createDocumentFragment();
+  self.appendElement("span", (self) => {
+    self.addClassName("gloss-word");
+    self.appendChild(transformer.apply(element, "page.gloss.li"));
+  });
+  return self;
+});
+
+manager.registerElementRule(["sh", "bs", "ex"], "page.gloss.li", (transformer, document, element) => {
+  let self = document.createDocumentFragment();
+  let className = (element.tagName === "sh") ? "gloss-name" : (element.tagName === "bs") ? "gloss-base" : "gloss-explanation";
+  self.appendElement("span", (self) => {
+    self.addClassName(className);
+    self.appendChild(transformer.apply(element, "page"));
+  });
+  return self;
+});
+
+manager.registerElementRule("mph", "page", (transformer, document, element) => {
+  let self = document.createDocumentFragment();
+  self.appendElement("span", (self) => {
+    self.addClassName("gloss-morpheme");
+    self.appendChild(transformer.apply(element, "page"));
+  });
+  return self;
+});
+
+manager.registerTextRule("page.gloss", (transformer, document, text) => {
+  let content = "";
+  if (text.previousSibling !== null && text.nextSibling !== null) {
+    let previousSibling = text.previousSibling;
+    let nextSibling = text.nextSibling;
+    if (previousSibling.isElement() && previousSibling.getAttribute("punc").match(/(\(|\[|«|“)$/)) {
+      content = "";
+    } else if (nextSibling.isElement() && nextSibling.getAttribute("punc").match(/^(\)|\.|,|:|;|·|!|\?)/)) {
+      content = "";
+    } else {
+      content = text.data;
+    }
+  } else {
+    content = "";
+  }
+  let self = document.createTextNode(content);
+  return self;
+});
+
 manager.registerElementRule("side", "page", (transformer, document, element) => {
   let self = document.createDocumentFragment();
   self.appendElement("figure", (self) => {
