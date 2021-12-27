a982338490e7b43325515cb3d76227b2b2b62e7b
Ziphil <yfe23457@nifty.com>
2021-12-27 16:31:06 +0900

    フォーム関係の変換規則を追加

--- template/block/form.ts
+++ template/block/form.ts
@@ -17,6 +17,60 @@ manager.registerElementRule("form", "page", (transformer, document, element) => 
   return self;
 });
 
+manager.registerElementRule("form-table", "page", (transformer, document, element) => {
+  let self = document.createDocumentFragment();
+  self.appendElement("table", (self) => {
+    self.addClassName("form-table");
+    self.setBlockType("bordered", "bordered");
+    self.appendChild(transformer.apply(element, "page.form-table"));
+  });
+  return self;
+});
+
+manager.registerElementRule("tr", "page.form-table", (transformer, document, element) => {
+  let self = document.createDocumentFragment();
+  self.appendElement("tr", (self) => {
+    self.appendChild(transformer.apply(element, "page.form-table.tr"));
+  });
+  return self;
+});
+
+manager.registerElementRule("td", "page.form-table.tr", (transformer, document, element) => {
+  let self = document.createDocumentFragment();
+  self.appendElement("td", (self) => {
+    self.appendChild(transformer.apply(element, "page"));
+  });
+  return self;
+});
+
+manager.registerElementRule("input", "page", (transformer, document, element) => {
+  let self = document.createDocumentFragment();
+  self.appendElement("input", (self) => {
+    self.addClassName("form-input");
+    self.setAttribute("type", "text");
+    if (element.hasAttribute("id")) {
+      self.setAttribute("id", element.getAttribute("id"));
+    }
+    if (element.hasAttribute("size")) {
+      self.setAttribute("size", element.getAttribute("size"));
+    }
+    self.appendChild(transformer.apply(element, "page"));
+  });
+  return self;
+});
+
+manager.registerElementRule("button", "page", (transformer, document, element) => {
+  let self = document.createDocumentFragment();
+  self.appendElement("button", (self) => {
+    if (element.hasAttribute("id")) {
+      self.setAttribute("id", element.getAttribute("id"));
+    }
+    self.addClassName("form-button");
+    self.appendChild(transformer.apply(element, "page"));
+  });
+  return self;
+});
+
 manager.registerElementRule(["a", "ae"], "page.form", (transformer, document, element) => {
   let self = document.createDocumentFragment();
   self.appendElement("a", (self) => {
