da9733a79f25066bbac667261a9cd6b2756f2ec5
Ziphil <yfe23457@nifty.com>
2022-04-16 18:39:56 +0900

    文法詳細の目次から該当項目にリダイレクトできるよう変更

---
+++ document/common/program/script/17.ts
@@ -0,0 +1,30 @@
+/// <reference path="../../../../node_modules/typescript/lib/lib.dom.d.ts"/>
+/// <reference path="../../../../node_modules/typescript/lib/lib.dom.iterable.d.ts"/>
+
+import REFERENCE_INDEX_JSON from "../../../../log/reference/ja.json";
+import {
+  BaseExecutor
+} from "./module/executor";
+
+
+export class Executor extends BaseExecutor {
+
+  protected prepare(): void {
+    window.addEventListener("hashchange", () => {
+      this.jump();
+    });
+    this.jump();
+  }
+
+  private jump(): void {
+    const tag = window.location.hash.replace(/^#/, "");
+    const href = REFERENCE_INDEX_JSON.hrefs[tag];
+    if (href) {
+      window.location.replace(href);
+    }
+  }
+
+}
+
+
+Executor.regsiter();
