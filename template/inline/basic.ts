1265ef5fa8c46b42c89952c3bd4b54a9616cce16
Ziphil <yfe23457@nifty.com>
2022-05-14 15:47:04 +0900

    シャレイア語部分を単語に分解して出力するよう変更

--- template/inline/basic.ts
+++ template/inline/basic.ts
@@ -7,11 +7,75 @@ import {
 
 let manager = new AvendiaTemplateManager();
 
+function splitToWords(string: string): Array<string> {
+  let splitStrings = ["", ...string.split(/('| )/), ""];
+  for (let i = 0 ; i < splitStrings.length ; i ++) {
+    if (i % 2 === 0) {
+      if (splitStrings[i] === "'") {
+        let previous = splitStrings[i - 1];
+        if (previous === "s" || previous === "di" || previous === "ac" || previous === "al") {
+          splitStrings[i - 1] = splitStrings[i - 1] + "'";
+        } else {
+          splitStrings[i + 1] = "'" + splitStrings[i + 1];
+        }
+        splitStrings[i] = "";
+      }
+    } else {
+      let match;
+      if ((match = splitStrings[i].match(/^((?:\(|\[|«|“)+)(.*?)$/)) !== null) {
+        splitStrings[i - 1] = splitStrings[i - 1] + match[1];
+        splitStrings[i] = match[2];
+      }
+      if ((match = splitStrings[i].match(/^(.*?)((?:\)|\]|»|”|\.|,|:|;|!|\?)+)$/)) !== null) {
+        splitStrings[i + 1] = match[2] + splitStrings[i + 1];
+        splitStrings[i] = match[1];
+      }
+    }
+  }
+  return splitStrings;
+};
+
 manager.registerElementRule("x", true, (transformer, document, element) => {
   let self = document.createDocumentFragment();
   self.appendElement("span", (self) => {
     self.addClassName("sans");
-    self.appendChild(transformer.apply());
+    if (true) {
+      let currentNode = document.createDocumentFragment();
+      let currentName = "";
+      let appendCurrentNode = function () {
+        self.appendElement("span", (self) => {
+          self.addClassName("word");
+          self.setAttribute("data-name", currentName);
+          self.appendChild(currentNode);
+        });
+        currentNode = document.createDocumentFragment();
+        currentName = "";
+      };
+      for (let i = 0 ; i < element.childNodes.length ; i ++) {
+        let child = element.childNodes.item(i)!;
+        if (child.isText()) {
+          let splitContents = splitToWords(child.data);
+          for (let j = 0 ; j < splitContents.length ; j ++) {
+            let content = splitContents[j];
+            if (j % 2 === 1) {
+              currentNode.appendChild(document.createTextNode(content));
+              currentName += content;
+              if (j < splitContents.length - 2 || splitContents[j + 1] !== "") {
+                appendCurrentNode();
+              }
+            } else {
+              self.appendTextNode(content);
+            }
+          }
+        } else if (child.isElement()) {
+          currentNode.appendChild(transformer.processElement(child));
+          currentName += child.textContent;
+        }
+      }
+      appendCurrentNode();
+    } else {
+      self.appendChild(transformer.apply());
+    }
   });
   return self;
 });
