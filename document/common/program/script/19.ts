af1de052588b4f527d97e4a9a869980f8961e63c
Ziphil <yfe23457@nifty.com>
2023-01-05 21:57:29 +0900

    楽曲情報をHTMLで指定するよう変更

--- document/common/program/script/19.ts
+++ document/common/program/script/19.ts
@@ -16,34 +16,35 @@ import {
 } from "./player-module/player-pane";
 
 
-const SONG_SPECS = [
-  {
-    number: 1,
-    title: {shaleian: "xalíh acís", normal: "Le premier sentiment"},
-    date: "2022/12/30",
-    length: 101,
-    url: "https://drive.google.com/uc?export=download&id=1Bczin3WJtmW2y37MMwoDWbH3iiirMi78",
-    description: "最初の曲。"
-  },
-  {
-    number: 2,
-    title: {shaleian: "saltefac e'c a kut", normal: "Ils t'accueillent"},
-    date: "2023/01/01",
-    length: 35,
-    url: "https://drive.google.com/uc?export=download&id=1YzVkpjFIK3xiorbE2d34U2QRU2nlnLPr",
-    description: "ディミニッシュセブンス使いたくて始めたけど、途中でなんかよく分かんなくなった･･･。"
-  }
-] as Array<SongSpec>;
-
-
 export class Executor extends BaseExecutor {
 
   protected prepare(): void {
     const container = document.getElementById("main");
     if (container) {
       const root = createRoot(container);
-      root.render(createElement(PlayerList, {specs: SONG_SPECS}));
+      const specs = this.getSongSpecs();
+      console.log(specs);
+      root.render(createElement(PlayerList, {specs}));
     };
+  }
+
+  private getSongSpecs(): Array<SongSpec> {
+    const elements = document.querySelectorAll<HTMLElement>("zp-song");
+    const specs = [...elements].map((element) => {
+      const titleShaleian = element.querySelector<HTMLElement>("zp-title-shaleian")?.innerText;
+      const titleNormal = element.querySelector<HTMLElement>("zp-title-normal")?.innerText;
+      const description = element.querySelector<HTMLElement>("zp-description")?.innerText;
+      const spec = {
+        number: parseInt(element.getAttribute("number") ?? "0"),
+        title: (titleShaleian && titleNormal) ? {shaleian: titleShaleian, normal: titleNormal} : null,
+        date: element.getAttribute("date") ?? "",
+        length: parseInt(element.getAttribute("length") ?? "0"),
+        url: element.getAttribute("url") ?? "",
+        description: description ?? ""
+      };
+      return spec;
+    });
+    return specs;
   }
 
 }
