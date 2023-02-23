8a44292fd388b5921eea324dff8ac1a74084deba
Ziphil <yfe23457@nifty.com>
2023-02-23 14:46:05 +0900

    音楽記号を入力する要素を作成

--- template/inline/basic.ts
+++ template/inline/basic.ts
@@ -5,6 +5,17 @@ import {
 } from "../../generator/transformer";
 
 
+const MUSIC_SYMBOL_SPECS = new Map([
+  ["s", ["music-accidental", "sharp", "♯"]],
+  ["f", ["music-accidental", "flat", "♭"]],
+  ["n", ["music-accidental", "natural", "♮"]],
+  ["maj", ["music-chord", "major", "Δ"]],
+  ["min", ["music-chord", "minor", "−"]],
+  ["aug", ["music-chord", "augmented", "+"]],
+  ["dim", ["music-chord", "diminished", "o"]],
+  ["hdim", ["music-chord", "half-diminished", "ø"]]
+]);
+
 const manager = new AvendiaTemplateManager();
 
 function splitToWords(string: string): Array<string> {
@@ -137,6 +148,18 @@ manager.registerElementRule("ch", true, (transformer, document, element) => {
       self.appendTextNode(String.fromCodePoint(0xA0));
     }
   }
+  return self;
+});
+
+manager.registerElementRule("ms", true, (transformer, document, element) => {
+  const self = document.createDocumentFragment();
+  const rawType = element.getAttribute("t");
+  const [className, type, text] = MUSIC_SYMBOL_SPECS.get(rawType)!;
+  self.appendElement("span", (self) => {
+    self.addClassName(className);
+    self.setAttribute("data-type", type);
+    self.appendTextNode(text);
+  });
   return self;
 });
 
