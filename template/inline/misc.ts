16070e9a9e4a6b32b2c350b3ed4a1d823306b263
Ziphil <yfe23457@nifty.com>
2023-06-10 12:19:13 +0900

    本文内の箇条書き参照用ボックスに数値を入れられるよう変更

--- template/inline/misc.ts
+++ template/inline/misc.ts
@@ -11,6 +11,12 @@ manager.registerElementRule("box", "page", (transformer, document, element) => {
   const self = document.createDocumentFragment();
   self.appendElement("span", (self) => {
     self.addClassName("box");
+    if (element.hasAttribute("num")) {
+      self.appendTextNode(element.getAttribute("num"));
+    }
+    if (element.hasAttribute("tag")) {
+      self.appendTextNode(element.getAttribute("tag").toUpperCase());
+    }
     self.appendChild(transformer.apply());
   });
   return self;
