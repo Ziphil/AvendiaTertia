ab42e2499f8f2c86085c44003d2c6b7cdc8c609e
Ziphil <yfe23457@nifty.com>
2024-05-14 22:48:18 +0900

    RTLの段落を表示できるよう変更

--- template/block/paragraph.ts
+++ template/block/paragraph.ts
@@ -13,6 +13,10 @@ manager.registerElementRule("p", "page", (transformer, document, element) => {
     self.addClassName("paragraph");
     self.setBlockType("text", "text");
     self.appendChild(transformer.apply());
+    if (element.hasAttribute("rtl")) {
+      self.setAttribute("dir", "rtl");
+      self.setAttribute("data-rtl", "");
+    }
     if (element.hasAttribute("num")) {
       self.insertHead(document.createElement("span", (self) => {
         self.addClassName("paragraph-number");
