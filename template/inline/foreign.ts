1c14cc260202db60babdf2c5581ee28fce85f12e
Ziphil <yfe23457@nifty.com>
2025-07-06 16:23:56 +0900

    ヒエログリフ関連の処理を外部パッケージで行うよう変更

--- template/inline/foreign.ts
+++ template/inline/foreign.ts
@@ -1,7 +1,7 @@
 //
 
+import {convertExtendedMdcToUnicode} from "hieroglyph-utils";
 import {AvendiaTemplateManager} from "../../generator/transformer";
-import HIEROGLYPH_DATA from "../hieroglyph.json";
 
 
 const manager = new AvendiaTemplateManager();
@@ -44,7 +44,7 @@ manager.registerElementRule("hrs", "page", (transformer, document, element) => {
       if (element.hasAttribute("rtl")) {
         self.setAttribute("data-dir", "hrl");
       }
-      self.appendTextNode(convertExtendedMdc(element.textContent ?? ""));
+      self.appendTextNode(convertExtendedMdcToUnicode(element.textContent ?? ""));
     });
   });
   return self;
@@ -61,43 +61,10 @@ manager.registerElementRule("hr", "page", (transformer, document, element) => {
       if (element.hasAttribute("rtl")) {
         self.setAttribute("data-dir", "hrl");
       }
-      self.appendTextNode(convertExtendedMdc(element.textContent ?? ""));
+      self.appendTextNode(convertExtendedMdcToUnicode(element.textContent ?? ""));
     });
   });
   return self;
 });
-
-function convertExtendedMdc(string: string): string {
-  const result = string.split(/(\s|-|:|\*|\(|\)|<|>|@\w+?@)/).map((part) => {
-    if (part.match(/\s/) || part === "-") {
-      return "";
-    } else if (part === ":") {
-      return "\uD80D\uDC30";
-    } else if (part === "*") {
-      return "\uD80D\uDC31";
-    } else if (part === "(") {
-      return "\uD80D\uDC37";
-    } else if (part === ")") {
-      return "\uD80D\uDC38";
-    } else if (part === "<") {
-      return "\uD80C\uDF79\uD80D\uDC3C";
-    } else if (part === ">") {
-      return "\uD80D\uDC3D\uD80C\uDF7A";
-    } else if (part === "@TS@") {
-      return "\uD80D\uDC32";
-    } else if (part === "@BS@") {
-      return "\uD80D\uDC33";
-    } else if (part === "@TE@") {
-      return "\uD80D\uDC34";
-    } else if (part === "@BE@") {
-      return "\uD80D\uDC35";
-    } else if (part === "@M@") {
-      return "\uD80D\uDC36";
-    } else {
-      return (HIEROGLYPH_DATA.glyphs as any)[part] ?? "";
-    }
-  });
-  return result.join("");
-}
 
 export default manager;
