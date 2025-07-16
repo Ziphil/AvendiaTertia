c118e30aa2edc9697f2f6cc40c0491d5dd9ad1d0
Ziphil <yfe23457@nifty.com>
2025-07-16 19:49:52 +0900

    フェンナ語データの取得先を変更

--- document/common/program/script/fennese/word.ts
+++ document/common/program/script/fennese/word.ts
@@ -25,17 +25,8 @@ export interface Dictionary {
 
 
 export async function getDictionary(): Promise<Dictionary> {
-  const apiKey = location.search.match(/apiKey=([^&]+)/)?.[1] ?? null;
-  if (apiKey !== null) {
-    return await getDictionaryFromApi(apiKey);
-  } else {
-    return await getDictionaryFromFile();
-  }
-}
-
-export async function getDictionaryFromApi(apiKey: string): Promise<Dictionary> {
   const fetchRawWords = async function (page: number): Promise<[Array<any>, number]> {
-    const response = await fetch(`https://zpdic.ziphil.com/api/v0/dictionary/fennese/words?text=&skip=${page * 100}&limit=100`, {headers: {"X-Api-Key": apiKey}});
+    const response = await fetch(`https://script.google.com/macros/s/AKfycbxXib3bXPFx0eYP_No0Kb2YVtFriB6t2KGqiRnLoeIx3DRtcgjpBbf5aWgN0Vhmqg/exec?text=&skip=${page * 100}&limit=100`);
     const json = await response.json();
     return [json["words"], json["total"]];
   };
@@ -45,38 +36,14 @@ export async function getDictionaryFromApi(apiKey: string): Promise<Dictionary> 
     return lastRawWords;
   }));
   const rawWords = [...firstRawWords, ...lastRawWords.flat()];
-  const words = rawWords.map(createWordFromZpdic);
-  const rootCount = calcRootCountFromZpdic(rawWords);
-  const wordCount = calcWordCountFromZpdic(rawWords);
+  const words = rawWords.map(createWord);
+  const rootCount = calcRootCount(rawWords);
+  const wordCount = calcWordCount(rawWords);
   console.log(`Words fetched from api: ${rootCount} roots, ${wordCount} words`);
   return {words, rootCount, wordCount};
 }
 
-export async function getDictionaryFromFile(): Promise<Dictionary> {
-  const response = await fetch("/file/dictionary/2.json");
-  const json = await response.json();
-  const words = json["words"].map(createWordFromOtm);
-  const rootCount = calcRootCountFromOtm(json["words"]);
-  const wordCount = calcWordCountFromOtm(json["words"]);
-  console.log(`Words fetched from file: ${rootCount} roots, ${wordCount} words`);
-  return {words, rootCount, wordCount};
-}
-
-function createWordFromOtm(rawWord: any): Word {
-  const rawTranslations = rawWord["translations"] as Array<any>;
-  const rawRelations = rawWord["relations"] as Array<any>;
-  const word = {
-    number: +rawWord["entry"]["id"],
-    name: rawWord["entry"]["form"],
-    equivalents: rawTranslations.map((rawTranslation) => rawTranslation["forms"][0]),
-    root: parseRoot(rawRelations.filter((rawRelation) => rawRelation["title"] === "語根").map((rawRelation) => rawRelation["entry"]["form"])[0]),
-    pattern: parsePatternFromOtm(rawRelations.filter((rawRelation) => rawRelation["title"] === "語型")),
-    affixes: parseAffixesFromOtm(rawRelations.filter((rawRelation) => rawRelation["title"] === "語型"))
-  };
-  return word;
-}
-
-function createWordFromZpdic(rawWord: any): Word {
+function createWord(rawWord: any): Word {
   const rawEquivalents = rawWord["equivalents"] as Array<any>;
   const rawRelations = rawWord["relations"] as Array<any>;
   const word = {
@@ -84,8 +51,8 @@ function createWordFromZpdic(rawWord: any): Word {
     name: rawWord["name"],
     equivalents: rawEquivalents.map((rawEquivalent) => rawEquivalent["names"][0]),
     root: parseRoot(rawRelations.filter((rawRelation) => rawRelation["titles"][0] === "語根").map((rawRelation) => rawRelation["name"])[0]),
-    pattern: parsePatternFromZpdic(rawRelations.filter((rawRelation) => rawRelation["titles"][0] === "語型")),
-    affixes: parseAffixesFromZpdic(rawRelations.filter((rawRelation) => rawRelation["titles"][0] === "語型"))
+    pattern: parsePattern(rawRelations.filter((rawRelation) => rawRelation["titles"][0] === "語型")),
+    affixes: parseAffixes(rawRelations.filter((rawRelation) => rawRelation["titles"][0] === "語型"))
   };
   return word;
 }
@@ -103,17 +70,7 @@ function parseRoot(rawRoot: string | undefined): WordRoot | null {
   }
 }
 
-function parsePatternFromOtm(rawRelations: Array<any>): WordPattern | null {
-  const patterns = rawRelations.map((rawRelation) => {
-    const rawForm = rawRelation["entry"]["form"];
-    const pattern = (DATA.patterns as any)[rawForm] ?? null;
-    return pattern;
-  });
-  const pattern = patterns.find((pattern) => pattern !== null) ?? null;
-  return pattern;
-}
-
-function parsePatternFromZpdic(rawRelations: Array<any>): WordPattern | null {
+function parsePattern(rawRelations: Array<any>): WordPattern | null {
   const patterns = rawRelations.map((rawRelation) => {
     const rawForm = rawRelation["name"];
     const pattern = (DATA.patterns as any)[rawForm] ?? null;
@@ -123,24 +80,7 @@ function parsePatternFromZpdic(rawRelations: Array<any>): WordPattern | null {
   return pattern;
 }
 
-function parseAffixesFromOtm(rawRelations: Array<any>): Array<string> {
-  const affixes = rawRelations.map((rawRelation) => {
-    const rawForm = rawRelation["entry"]["form"];
-    if ((DATA.patterns as any)[rawForm] === undefined) {
-      const affix = rawForm.match(/^‹(.*)›$/)?.[1] ?? null;
-      if (affix !== null && !affix.startsWith("=")) {
-        return affix;
-      } else {
-        return null;
-      }
-    } else {
-      return null;
-    }
-  }).filter((affix) => affix !== null);
-  return affixes;
-}
-
-function parseAffixesFromZpdic(rawRelations: Array<any>): Array<string> {
+function parseAffixes(rawRelations: Array<any>): Array<string> {
   const affixes = rawRelations.map((rawRelation) => {
     const rawForm = rawRelation["name"];
     if ((DATA.patterns as any)[rawForm] === undefined) {
@@ -157,19 +97,7 @@ function parseAffixesFromZpdic(rawRelations: Array<any>): Array<string> {
   return affixes;
 }
 
-function calcRootCountFromOtm(rawWords: Array<any>): number {
-  const roots = new Set<string>();
-  for (const rawWord of rawWords) {
-    const rawRelations = rawWord["relations"] as Array<any>;
-    const rawRoot = rawRelations.filter((rawRelation) => rawRelation["title"] === "語根").map((rawRelation) => rawRelation["entry"]["form"])[0];
-    if (rawRoot?.match(/^√(.+)-(.+)-(.+)$/)) {
-      roots.add(rawRoot);
-    }
-  }
-  return roots.size;
-}
-
-function calcRootCountFromZpdic(rawWords: Array<any>): number {
+function calcRootCount(rawWords: Array<any>): number {
   const roots = new Set<string>();
   for (const rawWord of rawWords) {
     const rawRelations = rawWord["relations"] as Array<any>;
@@ -181,13 +109,7 @@ function calcRootCountFromZpdic(rawWords: Array<any>): number {
   return roots.size;
 }
 
-function calcWordCountFromOtm(rawWords: Array<any>): number {
-  const otherCount = rawWords.filter((rawWord) => rawWord["tags"].includes("語根") || rawWord["tags"].includes("語型") || rawWord["tags"].includes("語型接辞")).length;
-  const wordCount = rawWords.length - otherCount;
-  return wordCount;
-}
-
-function calcWordCountFromZpdic(rawWords: Array<any>): number {
+function calcWordCount(rawWords: Array<any>): number {
   const otherCount = rawWords.filter((rawWord) => rawWord["tags"].includes("語根") || rawWord["tags"].includes("語型") || rawWord["tags"].includes("語型接辞")).length;
   const wordCount = rawWords.length - otherCount;
   return wordCount;
