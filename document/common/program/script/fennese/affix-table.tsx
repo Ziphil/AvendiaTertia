72631733d972a82c93188cc58e9fd25af23446ab
Ziphil <yfe23457@nifty.com>
2025-07-25 23:14:00 +0900

    前置詞の一覧も表示するよう変更

--- document/common/program/script/fennese/affix-table.tsx
+++ document/common/program/script/fennese/affix-table.tsx
@@ -1,7 +1,7 @@
 /// <reference path="../../../../../node_modules/typescript/lib/lib.dom.d.ts"/>
 /// <reference path="../../../../../node_modules/typescript/lib/lib.dom.iterable.d.ts"/>
 
-import {AFFIX_TYPES, AffixType, AffixWord, Word, getAffixType} from "ogorasso";
+import {AFFIX_TYPES, AffixType, AffixWord, NormalWord, Word, getAffixType} from "ogorasso";
 import {ReactElement} from "react";
 import AffixView from "./affix-view";
 import {Dictionary} from "./word";
@@ -18,7 +18,7 @@ const AffixTable = function ({
   const node = (
     <article className="affix-table">
       <div className="affix-header-row">
-        {AFFIX_TYPES.map((affixType) => (
+        {EXTENDED_AFFIX_TYPES.map((affixType) => (
           <div key={affixType} className="affix-header-cell">
             <span>
               {(affixType === "prestem") ? (
@@ -29,13 +29,15 @@ const AffixTable = function ({
                 <>幹母音後</>
               ) : (affixType === "poststem") ? (
                 <>語幹後</>
+              ) : (affixType === "preposition") ? (
+                <>前置詞</>
               ) : null}
             </span>
           </div>
         ))}
       </div>
       <div className="affix-row">
-        {AFFIX_TYPES.map((affixType) => (
+        {EXTENDED_AFFIX_TYPES.map((affixType) => (
           <div key={affixType} className="affix-cell">
             {affixWords[affixType].map((word) => (
               <AffixView key={word.toString()} word={word}/>
@@ -50,13 +52,16 @@ const AffixTable = function ({
 };
 
 
-function getAffixWords(words: Array<Word>): Record<AffixType, Array<AffixWord>> {
+const EXTENDED_AFFIX_TYPES = [...AFFIX_TYPES, "preposition"] as const;
+
+function getAffixWords(words: Array<Word>): Record<AffixType, Array<AffixWord>> & {preposition: Array<NormalWord>} {
   const affixWords = {
-    prestem: [],
-    prethematic: [],
-    postthematic: [],
-    poststem: []
-  } as Record<AffixType, Array<AffixWord>>;
+    prestem: [] as Array<AffixWord>,
+    prethematic: [] as Array<AffixWord>,
+    postthematic: [] as Array<AffixWord>,
+    poststem: [] as Array<AffixWord>,
+    preposition: [] as Array<NormalWord>
+  };
   for (const word of words) {
     if (word.kind === "affix") {
       const affixType = getAffixType(word.affix);
@@ -64,10 +69,14 @@ function getAffixWords(words: Array<Word>): Record<AffixType, Array<AffixWord>> 
         affixWords[affixType].push(word);
       }
     }
+    if (word.kind === "normal" && word.equivalents[0]?.titles[0] === "前置詞") {
+      affixWords.preposition.push(word);
+    }
   }
   for (const affixType of AFFIX_TYPES) {
     affixWords[affixType].sort((first, second) => first.affix.length - second.affix.length);
   }
+  affixWords.preposition.sort((first, second) => first.form.length - second.form.length);
   return affixWords;
 }
 
--- document/common/program/script/fennese/affix-view.tsx
+++ document/common/program/script/fennese/affix-view.tsx
@@ -1,21 +1,21 @@
 /// <reference path="../../../../../node_modules/typescript/lib/lib.dom.d.ts"/>
 /// <reference path="../../../../../node_modules/typescript/lib/lib.dom.iterable.d.ts"/>
 
-import {AffixWord} from "ogorasso";
+import {AffixWord, NormalWord} from "ogorasso";
 import {ReactElement} from "react";
 
 
 const AffixView = function ({
   word
 }: {
-  word: AffixWord
+  word: AffixWord | NormalWord
 }): ReactElement {
 
   const dictionaryUrl = `https://zpdic.ziphil.com/dictionary/fennese?kind=exact&number=${word.number}`;
 
   const node = (
     <div className="word-view">
-      <a className="word-view-name sans" href={dictionaryUrl} target="_blank" rel="noopener noreferrer">{word.affix}</a>
+      <a className="word-view-name sans" href={dictionaryUrl} target="_blank" rel="noopener noreferrer">{("affix" in word) ? word.affix : word.form}</a>
       <div className="word-view-equivalent">{word.equivalents.map((equivalent) => equivalent.terms[0]).join(", ")}</div>
     </div>
   );
